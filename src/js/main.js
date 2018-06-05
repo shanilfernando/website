bde5b7f74db6533407382d8a4c06435c74b66579
Shanil Fernando <shanilfernando@hotmail.com>
2018-06-05 09:24:35 +0530

    Initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,7 @@
 /*global Xiuli*/
-new Xiuli('xiuli', Xiuli.initializers.steps);
+const xiuli = new Xiuli('xiuli', Xiuli.initializers.spiralRotated);
+
+document.getElementById('btn1').onclick = ev => xiuli.goto('section1', ev);
+document.getElementById('btn2').onclick = ev => xiuli.goto('section2', ev);
+document.getElementById('btn3').onclick = ev => xiuli.goto('section3', ev);
+document.getElementById('btn4').onclick = ev => xiuli.goto('section4', ev);
