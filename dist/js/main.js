255c471deb6571f27e67b78402e2ce6e19cdbd14
Shanil Fernando <shanilfernando@hotmail.com>
2018-06-07 09:17:26 +0530

    Initial commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,3 +5,18 @@ document.getElementById('btn1').onclick = ev => xiuli.goto('section1', ev);
 document.getElementById('btn2').onclick = ev => xiuli.goto('section2', ev);
 document.getElementById('btn3').onclick = ev => xiuli.goto('section3', ev);
 document.getElementById('btn4').onclick = ev => xiuli.goto('section4', ev);
+
+let slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName('mySlides');
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > x.length) { slideIndex = 1; }
+  x[slideIndex - 1].style.display = 'block';
+  setTimeout(carousel, 2000);
+}
