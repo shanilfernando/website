42fe5a5069576ff2fa40a58603e8a70d41fb0728
Shanil Fernando <shanilfernando@hotmail.com>
2018-06-15 20:19:29 +0530

    Initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -8,17 +8,23 @@ document.getElementById('btn4').onclick = ev => xiuli.goto('section4', ev);
 
 const indexes = {
   slides1: 0,
-  slides2: 0
+  slides2: 0,
+  typewriterline:0
 };
 carousel();
-
+typewriter();
 function carousel() {
-  animate('slides1', indexes);
-  animate('slides2', indexes);
+  animate('slides1');
+  animate('slides2');
   setTimeout(carousel, 4000);
 }
 
-function animate(csClass, indexes) {
+function typewriter() {
+  animate('typewriterline');
+  setTimeout(typewriter, 7000);
+}
+
+function animate(csClass) {
   var x = document.getElementsByClassName(csClass);
   var i;
   for (i = 0; i < x.length; i++) {
